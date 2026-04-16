c5b08472d206c44fbf241ca2dfd08b222fcd8e1e
super <97206982+SuperMo0@users.noreply.github.com>
2026-04-16 23:19:11 +0200

    refactor(router): remove unused cache middleware and clean up imports

--- Backend/Routes/movies.router.ts
+++ Backend/Routes/movies.router.ts
@@ -1,15 +1,8 @@
-import { Router, type Request, type Response, type NextFunction } from 'express'
+import { Router } from 'express'
 import * as controller from '../controllers/movies.controller.ts'
-import { getSecondsUntil3AM } from '../utils/cache.ts'
 
 const router = Router()
 
-router.use((req: Request, res: Response, next: NextFunction) => {
-  res.set('Cache-Control', `public, max-age=${getSecondsUntil3AM()}`)
-  next()
-})
-
-router.get('/', controller.getAllMovies)
 router.get('/today', controller.getTodayMovies)
 
 export default router
