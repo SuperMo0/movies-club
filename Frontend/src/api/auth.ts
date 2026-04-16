b8eba9dcc5b4e12b33284501b5013211699425a5
super <97206982+SuperMo0@users.noreply.github.com>
2026-04-16 20:50:28 +0200

    refactor(auth): remove local catchAsync function and import from utils

--- Frontend/src/api/auth.ts
+++ Frontend/src/api/auth.ts
@@ -1,19 +1,13 @@
 import { AxiosError, type AxiosResponse } from 'axios';
 import client from '../lib/axios.js';
 import type { ResponseSafeUser, LoginType, SignupType } from 'moviesclub-shared/auth';
+import { catchAsync } from '@/utils/catch-async.js';
 
 type SessionResponse = {
     user: null | ResponseSafeUser
 }
 
-const catchAsync = async <T>(promise: Promise<AxiosResponse<T>>) => {
-    try {
-        const response = await promise;
-        return [null, response.data] as [null, T];
-    } catch (error) {
-        return [error, null] as [AxiosError<{ message: string }>, null];
-    }
-};
+
 
 export const checkSession = async () => {
     const [error, data] = await catchAsync(client.get<SessionResponse>('/auth/check'));
