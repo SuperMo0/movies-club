ff0a6bef2df741ab776d7d8b98e1ff094c53298c
super <97206982+SuperMo0@users.noreply.github.com>
2026-04-16 23:12:37 +0200

    refactor(schema): clean up and update post creation schema

--- Shared/social.schema.ts
+++ Shared/social.schema.ts
@@ -1,24 +1,18 @@
 import { z } from 'zod';
 
-export const userIdParamSchema = z.object({
-    userId: z.string().min(1, "User ID is required")
-});
-
-export const postIdParamSchema = z.object({
-    postId: z.string().min(1, "Post ID is required")
-});
-
-export const commentBodySchema = z.object({
-    content: z.string().min(1, "Content is required")
-});
-
 export const createPostBodySchema = z.object({
     content: z.string().min(1, "Content is required"),
-    movieId: z.string().nullable().optional().or(z.literal('null')).or(z.literal('')),
-    rating: z.string().nullable().optional().or(z.literal('null')).or(z.literal(''))
-});
+    movieTitle: z.string().nullable().optional(),
+    rating: z.number().nullable().optional()
+}).refine((d) => !(d.rating && !d.movieTitle), { error: "Invalid input found a rating without a movieTitle" });
 
 export const updateProfileBodySchema = z.object({
     name: z.string().min(1, "Name cannot be empty"),
     bio: z.string().optional().nullable()
 });
+
+export const commentSchema = z.object({
+    content: z.string().min(1, "Comment cannot be empty")
+})
+
+export const IdSchema = z.uuidv4("Invalid id");
