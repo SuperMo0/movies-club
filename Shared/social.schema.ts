c522802f97654be981151f7601bb7d02289507eb
super <97206982+SuperMo0@users.noreply.github.com>
2026-04-17 22:00:36 +0200

    refactor(schema): restructure comment and post schemas

--- Shared/social.schema.ts
+++ Shared/social.schema.ts
@@ -11,8 +11,34 @@ export const updateProfileBodySchema = z.object({
     bio: z.string().optional().nullable()
 });
 
-export const commentSchema = z.object({
+export const createCommentBodySchema = z.object({
     content: z.string().min(1, "Comment cannot be empty")
 })
 
 export const IdSchema = z.uuidv4("Invalid id");
+
+
+export const commentSchema = z.object({
+    id: z.uuidv4(),
+    content: z.string(),
+    authorId: z.uuidv4(),
+    createdAt: z.string(),
+    postId: z.uuidv4()
+});
+
+export const postSchema = z.object({
+    id: z.uuidv4(),
+    content: z.string(),
+    movieTitle: z.string().nullish().optional(),
+    rating: z.number().nullish().optional(),
+    image: z.url().nullish().nullable(),
+    authorId: z.uuidv4(),
+    createdAt: z.string(),
+    _count: z.object({
+        likedBy: z.number()
+    }),
+    comments: z.array(commentSchema)
+});
+
+export type Post = z.infer<typeof postSchema>
+export type Comment = z.infer<typeof commentSchema>
