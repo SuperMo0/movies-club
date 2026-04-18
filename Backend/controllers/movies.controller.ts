4b69b0b2f9fbeffbcebfe9a7e106d44e5f1b6806
super <97206982+SuperMo0@users.noreply.github.com>
2026-04-18 18:37:02 +0200

    refactor(movies): temporary fix to duplicated movies

--- Backend/controllers/movies.controller.ts
+++ Backend/controllers/movies.controller.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from 'express'
 import { prisma } from './../lib/prisma.ts'
 import { getSecondsUntil3AM } from '../utils/cache.ts';
 
-let cachedMovies: any = null
+let cachedMovies: any[] = []
 let lastUpdate: string | null = null
 
 
@@ -18,9 +18,17 @@ export async function getTodayMovies(req: Request, res: Response) {
     return res.status(503).json({ message: "Movies are being fetched, please try again later." });
   }
 
-  let parsedData = JSON.parse(record.value);
+  let parsedData = JSON.parse(record.value) as any[];
 
-  cachedMovies = parsedData;
+  //temporary fix we have to check why movies are being duplicated
+  cachedMovies = []
+  const mp = new Map();
+  for (const m of parsedData) {
+    if (mp.has(m.id)) continue;
+    mp.set(m.id, true);
+    cachedMovies.push(m);
+  }
+
   lastUpdate = today;
 
   res.set('Cache-Control', `public, max-age=${Math.min(getSecondsUntil3AM(), 60 * 60)}`);
