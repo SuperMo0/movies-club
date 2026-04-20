090ca558e5e2c822c7a0f7d05c78a4282a120f80
super <97206982+SuperMo0@users.noreply.github.com>
2026-04-20 15:32:22 +0200

    refactor: update catch async function for better return type

--- frontend/src/utils/catch-async.ts
+++ frontend/src/utils/catch-async.ts
@@ -1,10 +1,14 @@
-import type { AxiosError, AxiosResponse } from "axios";
+import { AxiosError, type AxiosResponse } from "axios";
+
+
+// type x<T> = Promise<[null, T]> | Promise<[AxiosError<{ message: string }>, null]>;
 
 export const catchAsync = async <T>(promise: Promise<AxiosResponse<T>>) => {
     try {
         const response = await promise;
-        return [null, response.data] as [null, T];
+        return [null, response.data] as const
     } catch (error) {
-        return [error, null] as [AxiosError<{ message: string }>, null];
+        if (error instanceof AxiosError) return [error, null] as const;
+        else return [error, null] as [Error, null];
     }
 };
