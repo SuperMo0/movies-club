83bca533ad466c5a8f13427a32aa322c997eb8e0
super <97206982+SuperMo0@users.noreply.github.com>
2026-04-20 22:34:37 +0200

    refactor: reorginize schema defination and add missing schemas

--- shared/social.schema.ts
+++ shared/social.schema.ts
@@ -1,34 +1,12 @@
 import { z } from 'zod';
 
-export const createPostBodyClientSchema = z.object({
-    content: z.string().min(1, "Content is required"),
-    movieTitle: z.string().nullable().optional(),
-    rating: z.number().nullable().optional(),
-    image: z.file().optional().nullable().refine((f) => {
-        return !f || (f.type.startsWith('image'))
-    })
-}).refine((d) => !(d.rating && !d.movieTitle), { error: "Invalid input found a rating without a movieTitle" });
+// general schemas 
+export const IdSchema = z.uuidv4("Invalid id");
 
-
-export const createPostBodyServerSchema = z.object({
-    content: z.string().min(1, "Content is required"),
-    movieTitle: z.string().nullable().optional(),
-    rating: z.number().nullable().optional(),
-    image: z.url().optional().nullable()
-
-}).refine((d) => !(d.rating && !d.movieTitle), { error: "Invalid input found a rating without a movieTitle" });
-
-export const updateProfileBodySchema = z.object({
-    name: z.string().min(1, "Name cannot be empty"),
-    bio: z.string().optional().nullable()
-});
-
+// comment schemas
 export const createCommentBodySchema = z.object({
     content: z.string().min(1, "Comment cannot be empty")
 })
-
-export const IdSchema = z.uuidv4("Invalid id");
-
 
 export const commentSchema = z.object({
     id: z.uuidv4(),
@@ -38,6 +16,7 @@ export const commentSchema = z.object({
     postId: z.uuidv4()
 });
 
+// post schemas
 export const postSchema = z.object({
     id: z.uuidv4(),
     content: z.string(),
@@ -52,8 +31,44 @@ export const postSchema = z.object({
     comments: z.array(commentSchema)
 });
 
+export const createPostBodyClientSchema = z.object({
+    content: z.string().min(1, "Content is required"),
+    movieTitle: z.string().nullable().optional(),
+    rating: z.number().nullable().optional(),
+    image: z.file().optional().nullable().refine((f) => {
+        return !f || (f.type.startsWith('image'))
+    })
+}).refine((d) => !(d.rating && !d.movieTitle), { error: "Invalid input found a rating without a movieTitle" });
+
+export const createPostBodyServerSchema = z.object({
+    content: z.string().min(1, "Content is required"),
+    movieTitle: z.string().nullable().optional(),
+    rating: z.number().nullable().optional(),
+    image: z.url().optional().nullable()
+
+}).refine((d) => !(d.rating && !d.movieTitle), { error: "Invalid input found a rating without a movieTitle" });
+
+// update profile schemas
+export const updateProfileBodyBaseSchema = z.object({
+    name: z.string().min(1, "Name cannot be empty"),
+    bio: z.string().optional().nullable()
+});
+
+export const updateProfileBodyClientSchema = updateProfileBodyBaseSchema.extend({
+    image: z.file().nullable().optional().refine((f) => {
+        return !f || (f.type.startsWith('image'))
+    })
+})
+
+export const updateProfileBodyServerSchema = updateProfileBodyBaseSchema.extend({
+    image: z.url().nullable().optional()
+})
+
+
 export type Post = z.infer<typeof postSchema>
 export type Comment = z.infer<typeof commentSchema>
 export type CreateCommentBody = z.infer<typeof createCommentBodySchema>
 export type CreatePostBodyClient = z.infer<typeof createPostBodyClientSchema>
 export type CreatePostBodyServer = z.infer<typeof createPostBodyServerSchema>
+export type UpdateProfileBodyClient = z.infer<typeof updateProfileBodyClientSchema>
+export type UpdateProfileBodyServer = z.infer<typeof updateProfileBodyServerSchema>
