c585aeb0ffc5178436cc6015598b59261e6060e0
super <97206982+SuperMo0@users.noreply.github.com>
2026-04-22 18:45:47 +0200

    fix: reset error boundary state when user navigates

--- frontend/src/components/social-components/social-layout.tsx
+++ frontend/src/components/social-components/social-layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 import { ErrorBoundary } from 'react-error-boundary'
 import { QueryErrorResetBoundary } from '@tanstack/react-query'
 import { Button } from '../ui/button';
@@ -6,15 +6,17 @@ import { Wrench, WrenchIcon } from 'lucide-react'
 
 export default function SocialLayout() {
 
+    const location = useLocation();
+
     return (<QueryErrorResetBoundary>
         {({ reset }) => {
             return (
-                <ErrorBoundary onReset={reset}
+                <ErrorBoundary onReset={reset} resetKeys={[location.pathname]}
                     fallbackRender={({ error, resetErrorBoundary }) => {
                         return <div className='flex items-center justify-center mt-20'>
                             <div className='flex flex-col gap-2 items-center'>
                                 <Wrench size={100} className='animate-pulse text-primary-foreground stroke-1'></Wrench>
-                                <h1 className='text-2xl'>oops we ran into a problem! please try again later</h1>
+                                <h1 className='text-2xl'> <span className='text-primary'>O</span>ops we ran into a problem please try refreshing!</h1>
                                 <p>or you can contact me about the problem: moofk2002@gmail.com</p>
                                 <Button variant='ghost' onClick={resetErrorBoundary}>Refresh</Button>
                             </div>
@@ -25,7 +27,6 @@ export default function SocialLayout() {
                 </ErrorBoundary>
             )
         }}
-
 
     </QueryErrorResetBoundary>)
 }
