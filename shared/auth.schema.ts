466f62e595eac218aa57cd34e3a9cb5af7e5de26
super <97206982+SuperMo0@users.noreply.github.com>
2026-04-26 03:17:49 +0300

    refactor: enhance and fix variable name and code structure

--- shared/auth.schema.ts
+++ shared/auth.schema.ts
@@ -1,22 +1,34 @@
 import { z } from 'zod';
 
-export const SignupSchema = z.object({
-    name: z.string().min(2, "Name must be at least 2 characters long"),
-    username: z.string().min(3, "Username must be at least 3 characters long").max(20, "Username cannot exceed 20 characters"),
-    password: z.string().min(6, "Password must be at least 6 characters long"),
+
+
+export const signupSchema = z.object({
+    name: z.string()
+        .min(2, "Name must be at least 2 characters long"),
+    username: z.string()
+        .min(2, "Username must be at least 2 characters long")
+        .max(20, "Username cannot exceed 20 characters"),
+    password: z.string()
+        .min(3, "Password must be at least 3 characters long"),
 });
 
-export const LoginSchema = z.object({
-    username: z.string().min(1, "Username is required"),
-    password: z.string().min(1, "Password is required"),
+
+export const loginSchema = z.object({
+    username: z.string()
+        .min(2, "Username is required"),
+    password: z.string()
+        .min(3, "Password is required"),
 });
 
-export const ResponseSafeUserSchema = z.object({
+
+export const safeUserResponseSchema = z.object({
     id: z.string(),
     name: z.string(),
     username: z.string(),
-    image: z.nullish(z.url()),
-    bio: z.nullish(z.string()),
+    image: z.url()
+        .nullish(),
+    bio: z.string()
+        .nullish(),
     joinedAt: z.date(),
     _count: z.object({
         followedBy: z.number(),
@@ -24,15 +36,16 @@ export const ResponseSafeUserSchema = z.object({
     })
 })
 
+export type SafeUserResponse = z.infer<typeof safeUserResponseSchema>
+export type SignupType = z.infer<typeof signupSchema>;
+export type LoginType = z.infer<typeof loginSchema>;
 
 export type AuthUserResponse = {
-    user: ResponseSafeUser;
+    user: SafeUserResponse;
 };
 
 export type AuthSessionResponse = {
-    user: ResponseSafeUser | null;
+    user: SafeUserResponse | null;
 };
 
-export type ResponseSafeUser = z.infer<typeof ResponseSafeUserSchema>
-export type SignupType = z.infer<typeof SignupSchema>;
-export type LoginType = z.infer<typeof LoginSchema>;
+
