eff461dea8fb7419eafd7039d789ca023fb19744
super <97206982+SuperMo0@users.noreply.github.com>
2026-05-04 18:07:22 +0300

    refactor: use default cdn for image compression via web workers

--- frontend/src/utils/compress-image.ts
+++ frontend/src/utils/compress-image.ts
@@ -1,4 +1,3 @@
-
 import imageCompression from "browser-image-compression";
 
 export async function compressImage(image: File) {
@@ -6,7 +5,6 @@ export async function compressImage(image: File) {
         maxSizeMB: 0.1,
         maxWidthOrHeight: 1920,
         useWebWorker: true,
-        libURL: '/node_modules/browser-image-compression/dist/browser-image-compression.js?url'
     });
 
 }
